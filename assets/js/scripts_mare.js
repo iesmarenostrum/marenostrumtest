17aafd73855fa1aaca98a6132bcc589b98b55da0
Pedro Prieto <pedpral@gmail.com>
2017-05-25 20:09:00 +0200

    Línea de tiempo básica.

--- assets/js/scripts_mare.js
+++ assets/js/scripts_mare.js
@@ -24,6 +24,40 @@
     autocompletar.on('typeahead:selected', function (evt, data) {
       window.location = '{{ site.baseurl }}' + data.url;
     });
+
+    
+    /* -------------------------- */
+    // Línea de tiempo
+    // load data via an ajax request. When the data is in, load the timeline
+    $.ajax({
+      url: '{{ site.baseurl }}/test.json',
+      success: function (data) {
+        console.log(data);
+        // hide the "loading..." message
+        document.getElementById('loading').style.display = 'none';
+
+        // DOM element where the Timeline will be attached
+        var container = document.getElementById('visualization');
+
+        // Create a DataSet (allows two way data-binding)
+        var items = new vis.DataSet(data);
+
+        // Configuration for the Timeline
+        var options = {};
+
+        // Create a Timeline
+        var timeline = new vis.Timeline(container, items, options);
+      },
+      error: function (err) {
+        console.log('Error', err);
+        // if (err.status === 0) {
+        //   alert('Failed to load data/basic.json.\nPlease run this example on a server.');
+        // }
+        // else {
+        //   alert('Failed to load data/basic.json.');
+        // }
+      }
+    });
   });
 
 })(jQuery);
